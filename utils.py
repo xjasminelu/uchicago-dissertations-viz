2776552bd73cfe49445b9a6d20a81cdd50b5c3cd
Jasmine Lu <xjasminelu@gmail.com>
2026-01-26 14:19:04 -0600

    script to read in csv as json object and 2025 dissertation files'

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import csv
 import re
-
+import json
 from ast import literal_eval
 
 def load_csv(file):
@@ -24,13 +24,35 @@ def department_stats(data):
 				else:
 					dept_dict[dept] += 1
 
+			if len(dept_list) == 0:
+				# accounting for some cases where no department is listed just the school (Harris, Law, PME)
+				dept_list = literal_eval(entry[11])
+				for dept in dept_list:
+					if dept not in dept_dict.keys():
+						dept_dict[dept] = 1
+					else:
+						dept_dict[dept] += 1
+
 	sorted_dict = dict(sorted(dept_dict.items(), key= lambda item: item[1]))
 	for dept in sorted_dict:
 		print(dept)
 		print(sorted_dict[dept])
+
+def json_from_csv(csv_file, json_file):
+	csvfile = open(csv_file, 'r')
+	jsonfile = open(json_file, 'w')
+
+	fieldnames = ("Title","Date","Authors","Advisors","Committee Members","Department","Paper Categories","Paper Keywords")
+	reader = csv.DictReader( csvfile, fieldnames)
+	next(reader)
+	out = json.dumps( [ row for row in reader ] )
+	jsonfile.write(out)
 
 
 if __name__ == "__main__":
 	data = load_csv("knowledge_cleaned_for_tdm.csv")
 
 	department_stats(data)
+
+
+	#json_from_csv('2025_diss.csv', '2025_diss.json')
